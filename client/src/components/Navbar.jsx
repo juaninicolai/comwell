89008300537595f2b07cfb91a62495c331987989
Krishna Prasad Khanal <krishnaamen@gmail.com>
2023-11-03 00:40:19 +0100

    nextjs  setup for client

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+
+'use client'
+import React from 'react'
+function Navbar() {
+  return (
+    <nav className='bg-blue-400 h-12 py-2 px-3 flex justify-between'>
+        <div>left</div>
+        <div>middle</div>
+        <div>right</div>
+    </nav>
+  )
+}
+
+export default Navbar
