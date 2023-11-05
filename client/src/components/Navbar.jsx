c563e363892b9e3614e8c9a5ccf67811a7412fa7
Krishna Prasad Khanal <krishnaamen@gmail.com>
2023-11-05 15:37:34 +0100

    routing login page and signup page with search hotels UI

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,24 +1,25 @@
 
 'use client'
+import Link from "next/link"
 import React from 'react'
 function Navbar() {
   return (
       <nav className='bg-navColor-400 h-30 py-2 font-semibold px-3 flex justify-between'>
-        <div className="logo"> <a href="#!">
+        <div className="logo"> <Link href={'/'}>
         <img src="Logo.png" alt="logo" />
-        </a>
+        </Link>
         </div>
           
           
           <ul className='flex space-x-3'>
-            <li > <a href="#!">Overnight</a> </li>
-            <li > <a href="#!">Meetings & conference</a> </li>
-            <li > <a href="#!">Party</a> </li>
+            <li > <Link href={'/Search'}>Search Hotels</Link> </li>
+            <li > <Link href={'/'}>Offers and sale</Link> </li>
+            <li > <Link href={'/'}>Party</Link> </li>
             </ul>
             <ul className="flex space-x-3">
-            <li > <a href="#!">Locations</a> </li>
-            <li > <a href="#!">Profile</a> </li>
-            <li > <a href="#!">Menu</a> </li>
+            <li > <Link href={'/Location'}>Locations</Link> </li>
+            <li > <Link href={'/Login'}>Profile</Link> </li>
+            <li > <Link href={'/'}>Menu</Link> </li>
 
             </ul>
            
