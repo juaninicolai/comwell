1cb76d4763381b91ed7b3fbd98d940b97ae0af9d
Krishna Prasad Khanal <krishnaamen@gmail.com>
2023-11-08 10:13:41 +0100

    rendering hotels with dummy data,search UI

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,20 +4,19 @@ import Link from "next/link"
 import React from 'react'
 function Navbar() {
   return (
-      <nav className='bg-navColor-400 h-30 py-2 font-semibold px-3 flex justify-between'>
+
+    <header className="fixed top-0 w-full">
+        <nav className='  navbar h-30 py-2 font-semibold px-3 flex justify-between  '>
         <div className="logo"> <Link href={'/'}>
         <img src="Logo.png" alt="logo" />
         </Link>
         </div>
           
           
-          <ul className='flex space-x-3'>
-            <li > <Link className="hover:text-blue-300" href={'/Search'} >Search Hotels</Link> </li>
-            <li > <Link href={'/'}>Offers and sale</Link> </li>
-            <li > <Link href={'/'}>Party</Link> </li>
-            </ul>
+          
             <ul className="flex space-x-3">
-            <li > <Link href={'/Location'}>Locations</Link> </li>
+            <li > <Link  href={'/Search'} >Search Hotels</Link> </li>
+            <li > <Link href={'/Hotels'}>Locations</Link> </li>
             <li > <Link href={'/Login'}>Profile</Link> </li>
             <li > <Link href={'/'}>Menu</Link> </li>
 
@@ -28,6 +27,8 @@ function Navbar() {
 
         
     </nav>
+      </header> 
+
   )
 }
 
