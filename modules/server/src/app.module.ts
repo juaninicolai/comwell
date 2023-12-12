b2febe885025f8b9311154de76e88cc05eed478f
Veselin Ivanov <me@veselyn.com>
2023-12-12 18:08:17 +0100

    remove existing backend logic

--- modules/server/src/app.module.ts
+++ modules/server/src/app.module.ts
@@ -1,17 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { BookingModule } from './booking/booking.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://127.0.0.1:27017/hotel-booking'),
-    AuthModule,
-    BookingModule,
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/comwell'),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule { }
