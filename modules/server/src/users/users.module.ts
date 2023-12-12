91cc7023f942446dc6af3d565d73a398de9ef2e6
Veselin Ivanov <me@veselyn.com>
2023-12-12 19:43:02 +0100

    remove users controller

--- modules/server/src/users/users.module.ts
+++ modules/server/src/users/users.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './entities/user.entity';
 
@@ -8,7 +7,6 @@ import { User, UserSchema } from './entities/user.entity';
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
-  controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService]
 })
