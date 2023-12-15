9992f5a32c78040eb1a1ffe3787d14ff7fce142c
Veselin Ivanov <me@veselyn.com>
2023-12-15 16:56:06 +0100

    generate hotels resource

--- modules/server/src/app.module.ts
+++ modules/server/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { HotelsModule } from './hotels/hotels.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://127.0.0.1:27017/comwell'),
     UsersModule,
     AuthModule,
+    HotelsModule,
   ],
   controllers: [],
   providers: [],
