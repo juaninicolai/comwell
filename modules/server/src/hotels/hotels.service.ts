3bab7b7eef20fff8bf5223cb367b5d75863040c7
Veselin Ivanov <me@veselyn.com>
2023-12-15 17:31:38 +0100

    define hotels

--- modules/server/src/hotels/hotels.service.ts
+++ modules/server/src/hotels/hotels.service.ts
@@ -1,13 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { Hotel } from './entities/hotel.entity';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
-export class HotelsService {
+export class HotelsService implements OnModuleInit {
   constructor(@InjectModel(Hotel.name) private hotelModel: Model<Hotel>) { }
 
   findAll() {
-    return this.hotelModel.find()
+    return this.hotelModel.find();
+  }
+
+  async onModuleInit() {
+    const countOfHotels = await this.hotelModel.countDocuments();
+    if (countOfHotels > 0) {
+      return;
+    }
+
+    await this.hotelModel.insertMany([
+      { city: 'Aalborg' },
+      { city: 'Aarhus' },
+      { city: 'Copenhagen' },
+    ]);
   }
 }
