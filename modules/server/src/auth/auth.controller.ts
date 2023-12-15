605df05703c27680a7b7d048cd0b2383ad325fd4
Veselin Ivanov <me@veselyn.com>
2023-12-15 16:33:02 +0100

    create user decorator

--- modules/server/src/auth/auth.controller.ts
+++ modules/server/src/auth/auth.controller.ts
@@ -1,4 +1,5 @@
 import {
+  Get,
   Body,
   Controller,
   HttpException,
@@ -8,14 +9,15 @@ import {
 import { AuthService } from './auth.service';
 import { SignUpDto } from './dto/sign-up.dto';
 import { EmailIsTakenError } from 'src/users/errors/email-is-taken.error';
-import {LocalAuthGuard} from "./local-auth.guard";
+import { LocalAuthGuard } from "./local-auth.guard";
 import { Request } from 'express';
-import {UserDocument} from "../users/entities/user.entity";
-import {JwtAuthGuard} from "./jwt-auth.guard";
+import { UserDocument } from "../users/entities/user.entity";
+import { JwtAuthGuard } from "./jwt-auth.guard";
+import { User } from 'src/users/user.decorator';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService) { }
 
   @Post('signup')
   async signUp(@Body() signUpDto: SignUpDto) {
@@ -34,13 +36,13 @@ export class AuthController {
 
   @UseGuards(LocalAuthGuard)
   @Post('login')
-  logIn(@Req() req : Request) {
+  logIn(@Req() req: Request) {
     return this.authService.login(req.user as UserDocument);
   }
 
   @UseGuards(JwtAuthGuard)
-  @Post('profile')
-  getProfile(@Req() req : Request) {
-    return req.user;
+  @Get('profile')
+  getProfile(@User() user: UserDocument) {
+    return user;
   }
 }
