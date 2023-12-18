fc2517c6f1f161434ba789b8c64f57ec74436d99
juaninicolai <juan.nicolai@maersk.com>
2023-12-18 11:31:56 +0100

    Add validation docs comment:
    This needs to be removed.

--- modules/server/src/hotels/hotels.controller.ts
+++ modules/server/src/hotels/hotels.controller.ts
@@ -9,4 +9,12 @@ export class HotelsController {
   findAll() {
     return this.hotelsService.findAll();
   }
+
+  //In addition to validating request bodies, the ValidationPipe can be used with other request object properties as well.
+  // Imagine that we would like to accept :id in the endpoint path.
+  // To ensure that only numbers are accepted for this request parameter, we can use the following construct:
+  //@Get(':id')
+  // findOne(@Param() params: FindOneParams) {
+  //   return 'This action returns a user';
+  // }
 }
