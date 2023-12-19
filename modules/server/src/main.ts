8630eace5a81fc1137857f43f78d245d6b7caf76
juaninicolai <juan.nicolai@maersk.com>
2023-12-19 15:55:22 +0100

    Add swagger config

--- modules/server/src/main.ts
+++ modules/server/src/main.ts
@@ -1,11 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Comwell Hotels API')
+    .setDescription('The Comwell Hotels API description')
+    .setVersion('1.0')
+    .addTag('comwell')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3001);
 }
+
 bootstrap();
