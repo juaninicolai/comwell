277ea8cde2415f2e00e6b40b7b33315ea2cb31f1
juaninicolai <juan.nicolai@maersk.com>
2023-12-28 15:06:48 +0100

    Add user fields

--- modules/server/src/auth/dto/sign-up.dto.ts
+++ modules/server/src/auth/dto/sign-up.dto.ts
@@ -1,7 +1,21 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, IsDate } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
+import { Type } from "class-transformer";
 
 export class SignUpDto {
+  @ApiProperty()
+  @IsString()
+  firstName: string;
+
+  @ApiProperty()
+  @IsString()
+  lastName: string;
+
+  @ApiProperty()
+  @Type(() => Date)
+  @IsDate()
+  birthDate: Date;
+
   @ApiProperty()
   @IsEmail()
   email: string;
--- modules/server/src/users/dto/create-user.dto.ts
+++ modules/server/src/users/dto/create-user.dto.ts
@@ -1,9 +1,20 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import {IsDate, IsEmail, IsNotEmpty, IsString} from 'class-validator';
 
 export class CreateUserDto {
+
+  @IsString()
+  firstName: string;
+
+  @IsString()
+  lastName: string;
+
+  @IsDate()
+  birthDate: Date;
+
   @IsEmail()
   email: string;
 
   @IsNotEmpty()
   password: string;
+
 }
